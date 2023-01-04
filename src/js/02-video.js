60809d1b6c965c80ceaa477436601a23d0e1d164
Gefeste <gefeste111@gmail.com>
2023-01-04 18:47:58 +0100

    task-2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,28 @@
 import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
-console.log(Player)
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (currentTime) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds))
+}, 1000));
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+const secondTime = JSON.parse(savedTime);
+
+player.setCurrentTime(secondTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
