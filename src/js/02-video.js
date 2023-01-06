ce78032bde98ab85673fc18f95f0ab130395a967
Gefeste <gefeste111@gmail.com>
2023-01-06 11:03:57 +0100

    fix-hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,40 +11,6 @@ player.on('timeupdate', throttle(function (currentTime) {
 
 const savedTime = localStorage.getItem("videoplayer-current-time");
 const secondTime = JSON.parse(savedTime);
-
-player.setCurrentTime(secondTime).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const onPlay = function () {
-//     throttle(function(currentTime) {
-//         localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds))
-//     }, 1000)
-// }
-// console.log(onPlay())
-
-// player.on('timeupdate', onPlay)
-
-
+if (savedTime) {
+    player.setCurrentTime(secondTime)
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,6 +9,8 @@ returnTextValue()
 form.addEventListener('input', throttle(onFormInput, 500))
 
 function onFormInput(e) {
+
+
     obj[e.target.name] = e.target.value
    localStorage.setItem("feedback-form-state", JSON.stringify(obj))
 }
@@ -24,11 +26,14 @@ form.addEventListener('submit', (e) => {
 })
 
 function returnTextValue() {
-   const savedText = localStorage.getItem("feedback-form-state");
-    const textValue = JSON.parse(savedText);
-    if (textValue) {
-        email.value = textValue.email
-        input.value = textValue.message
+    const savedText = localStorage.getItem("feedback-form-state");
+    if (savedText) {
+        const textValue = JSON.parse(savedText);
+        Object.entries(textValue).forEach(([name, value]) => {
+            obj[name] = value;
+            form.elements[name].value = value;
+        })
     }
 }
+    
 
